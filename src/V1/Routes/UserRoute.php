64af12173ab2f0de0f1bd2876f45f0c74fa808ff
unknown <alexander.hoehling@alfenory.de>
2018-08-29 14:57:57 +0200

    Update roles

--- src/V1/Routes/UserRoute.php
+++ src/V1/Routes/UserRoute.php
@@ -5,8 +5,6 @@ namespace Alfenory\Auth\V1\Routes;
 class UserRoute {
     public function __construct($app) {
         $app->group("/user", function() {
-            $this->get('/', \Alfenory\Auth\V1\Controller\InfoController::class.':info');
-            $this->get('/session', \Alfenory\Auth\V1\Controller\UserController::class.":session");
             $this->get('/membership', \Alfenory\Auth\V1\Controller\UserController::class.":get_usergroup_membership");
             $this->get('/get_privileges', \Alfenory\Auth\V1\Controller\UserController::class.":get_privileges");
             $this->get('/login', \Alfenory\Auth\V1\Controller\UserController::class.":login");
