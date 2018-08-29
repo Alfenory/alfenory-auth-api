89e81341fc03e07052511d35e22def1f0b9c253d
unknown <alexander.hoehling@alfenory.de>
2018-08-29 14:02:50 +0200

    additional update roles

--- src/V1/Routes/RoleRoute.php
+++ src/V1/Routes/RoleRoute.php
@@ -9,12 +9,17 @@ class RoleRoute {
             $this->put('/{role_id}', \Alfenory\Auth\V1\Controller\RoleController::class.":update");
             $this->post('/', \Alfenory\Auth\V1\Controller\RoleController::class.":create");
             $this->delete("/", \Alfenory\Auth\V1\Controller\RoleController::class.":delete");
-            $this->group("/{role_id}/privileg", function() {
-                $this->get('', \Alfenory\Auth\V1\Controller\RolePrivilegController::class.":get");
+            $this->group("/privileg", function() {
+                $this->get('/', \Alfenory\Auth\V1\Controller\RolePrivilegController::class.":get");
+            });
+
+            $this->group("/roleprivileg/{role_id}", function() {
+                $this->get('/', \Alfenory\Auth\V1\Controller\RolePrivilegController::class.":get");
                 $this->put('/{privileg_id}', \Alfenory\Auth\V1\Controller\RolePrivilegController::class.":update");
                 $this->post('', \Alfenory\Auth\V1\Controller\RolePrivilegController::class.":update");
                 $this->delete("/{privileg_id}", \Alfenory\Auth\V1\Controller\RolePrivilegController::class.":delete");
             });
+
         });
 
         $app->group("/privileges/{membership_id}", function() {
